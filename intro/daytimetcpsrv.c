453dab5e5a555dce4686f1a31b154ce086472e5f
mrmodolo <marcelo.rmodolo@gmail.com>
2020-07-04 19:08:02 -0300

    clang-formater and __attribute__((unused))

--- intro/daytimetcpsrv.c
+++ intro/daytimetcpsrv.c
@@ -1,32 +1,28 @@
 #include	"unp.h"
-#include	<time.h>
+#include <time.h>
 
-int
-main(int argc, char **argv)
-{
-	int					listenfd, connfd;
-	struct sockaddr_in	servaddr;
-	char				buff[MAXLINE];
-	time_t				ticks;
+int main(__attribute__((unused)) int argc, __attribute__((unused)) char **arg) {
+  int listenfd, connfd;
+  struct sockaddr_in servaddr;
+  char buff[MAXLINE];
+  time_t ticks;
 
-	listenfd = Socket(AF_INET, SOCK_STREAM, 0);
+  listenfd = Socket(AF_INET, SOCK_STREAM, 0);
 
-	bzero(&servaddr, sizeof(servaddr));
-	servaddr.sin_family      = AF_INET;
-	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	servaddr.sin_port        = htons(13);	/* daytime server */
+  bzero(&servaddr, sizeof(servaddr));
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  servaddr.sin_port = htons(13); /* daytime server */
 
-	Bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
+  ind(listenfd, (SA *)&servaddr, sizeof(servaddr));
+  Listen(listenfd, LISTENQ);
 
-	Listen(listenfd, LISTENQ);
+  for (;;) {
+    connfd = Accept(listenfd, (SA *)NULL, NULL);
+    ticks = time(NULL);
+    snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
+    Write(connfd, buff, strlen(buff));
 
-	for ( ; ; ) {
-		connfd = Accept(listenfd, (SA *) NULL, NULL);
-
-        ticks = time(NULL);
-        snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
-        Write(connfd, buff, strlen(buff));
-
-		Close(connfd);
-	}
+    Close(connfd);
+  }
 }
